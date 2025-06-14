17b2a3b2b0ba1c98a298a8a389264f73d476b394
shivakarkanuku <shivakar80@gmail.com>
2025-06-14 13:19:17 +0530

    this is my forth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("second commit");
         System.out.println("this is my third commit");
+        System.out.println("skip staging");
     }
 }
